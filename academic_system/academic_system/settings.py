53a3f604977016316f9cbd2438f9ed6f48b13e84
thanuu26 <thanushree26m@gmail.com>
2026-03-23 22:34:56 +0530

    Moved requirements.txt to app root for Render

--- academic_system/academic_system/settings.py
+++ academic_system/academic_system/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-k+#c1i4=6-sn8#s_&j+mft^+1his(bthghyid_bv5l!y1@(ung
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -115,4 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
+import os # Make sure this is at the top of your file if it isn't already!
+
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') # <-- ADD THIS LINE
